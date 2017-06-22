758fece11f6284398fb96fc27be28c7e8ad71a11
Jake Burga <jrburga@mit.edu>
2017-06-22 16:52:29 -0400

    cleaned up code

--- pymunk-testcode/platformer_test.py
+++ pymunk-testcode/platformer_test.py
@@ -17,7 +17,6 @@ class Sprite(pygame.sprite.Sprite):
 		return [self.body, self.shape]
 
 	def update(self):
-		# print self.shape.collision_type
 		pass
 
 class Player(Sprite):
@@ -36,70 +35,49 @@ class Player(Sprite):
 		}
 
 		self.max_speed = 100*2.
-
 		self.shape.friction = 1.0
-		print self.shape.friction
-
-
-		self.target_vx = 0
-		self.jumping = False
-
 		self.grounded = False
-		# self.body.each_arbiter(self.collision_handler)
 
 	def ground_collision(self, arbiter):
+		# checks the normal vector between the player and the object it's colliding with
+		# if the slope is less than its coefficient of friction, it is grounded
 		N = arbiter.contact_point_set.normal
 		if arbiter.shapes[1].body:
 			if (N.y != 0 and abs(N.x/N.y) < self.shape.friction):
 				self.grounded = True
 
-			
-		# print 'something happened'
-		# return True
-
 	def walk(self, direction):
 		self.direction = direction
-
-		# self.target_vx = direction*self.max_speed
 		self.shape.surface_velocity = Vec2d(-self.max_speed*direction, 0)
-		# print self.shape.surface_velocity
-		# print 'walking', direction
-		# self.body.apply_impulse_at_local_point(Vec2d(direction*100, 0), (0, 0))
-		# self.body.angular_velocity = self.max_speed*direction
-		# self.body.velocity = (direction*self.max_speed, self.body.velocity.y)
+
 
 	def jump(self):
-		print self.grounded
+		# jump if the object is properly grounded
+		# set grounded to False
 		if self.grounded:
 			self.grounded = False
 			self.body.apply_impulse_at_local_point(Vec2d(0, 500))
 
 	def update(self):
-		# gravity
-		# print self.body.velocity
-		self.body.each_arbiter(self.ground_collision)
-		self.body.apply_force_at_local_point(self.gravity, (0, 0))
-		# self.body.angular_velocity = 0
-		# self.body.velocity = Vec2d(0, self.body.velocity.y)
-		self.shape.surface_velocity = Vec2d(0, 0)
 
-		# print self.shape.surface_
-		self.target_vx = 0
+		# check if object is properly "grounded"
+		self.body.each_arbiter(self.ground_collision)
+		# apply gravity
+		self.body.apply_force_at_local_point(self.gravity, (0, 0))
+		# reset surface velocity
+		self.shape.surface_velocity = Vec2d(0, 0)
+		# get key press and apply action
 		keys = pygame.key.get_pressed()
 		for player_key in self.actions:
 			if keys[player_key]:
-				# print player_key
 				self.actions[player_key]()
-		# print self.body.velocity
-		# self.shape.surface_velocity = Vec2d(-self.target_vx, 0)
-		# print self.shape.surface_velocity
-		# print self.shape.surface_velocity
-		
-		# print self.shape.surface_velocity
+
 
 
 
 class Platform(Sprite):
+	# creates a rectangle of the given size
+	# position is top left corner of the rectangle
 	def __init__(self, position, size):
 		self.body = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
 		p = Vec2d(position)
@@ -134,11 +112,14 @@ class Game():
 
 	def run_game(self, size):
 		pygame.init()
+
+		# initialize 
 		self.screen = pygame.display.set_mode(size)
 		self.clock = pygame.time.Clock()
 		self.fps = 60
 		self.dt = 1./self.fps
 
+		# add objects to the game world
 		player = Player((300, 220))
 		walls = []
 		walls.append(Platform((100, 200), (400, 20)))
@@ -149,25 +130,26 @@ class Game():
 		for wall in walls:
 			self.add_sprite(wall)
 
-		# player.body.each_arbiter(self.collision_handler)
-		# self.space.add_collision_handler(player.shape.collision_type, wall.shape.collision_type).begin = self.collision_handler
-
+		# set up debug draw mode
+		# actually displaying real graphics will take some more effort
+		# because the (0, 0) point is in the bottom left (unlike the pygame screen)
 		draw_options = pymunk.pygame_util.DrawOptions(self.screen)
 
 		self.running = True
 
+		# create a white background to refresh the screen
 		self.background = pygame.Surface(size)
 		self.background.fill(Color('white'))
+
 		while self.running:
+			# refresh the screen
 			self.screen.blit(self.background, (0, 0))
 			for event in pygame.event.get():
 				if event.type == QUIT:
 					self.running = False
 
 			self.sprites.update()
-			# print player.shape.surface_velocity
 			self.space.debug_draw(draw_options)			
-			# print player.shape.surface_velocity
 
 			self.space.step(self.dt)
 			self.clock.tick(self.fps)
