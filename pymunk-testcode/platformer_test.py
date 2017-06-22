8a4316b536e1bcbd1045458969edf2936f215289
Jake Burga <jrburga@mit.edu>
2017-06-22 17:37:45 -0400

    apples are cool

--- pymunk-testcode/platformer_test.py
+++ pymunk-testcode/platformer_test.py
@@ -10,23 +10,34 @@ import pymunk.pygame_util
 
 class Sprite(pygame.sprite.Sprite):
 
-	def __init__(self, pymunk_body):
+	def __init__(self, position, size):
 		pygame.sprite.Sprite.__init__(self)
-
+		self.image = pygame.Surface(size)
+		self.image.fill((0, 0, 0))
+		self.rect = self.image.get_rect()
 	def get_bodies(self):
 		return [self.body, self.shape]
 
-	def update(self):
-		pass
+	def update(self, screen):
+		self.pymunk2pygame(screen)
+
+	def pymunk2pygame(self, screen):
+		self.rect.x, self.rect.y = pymunk.pygame_util.to_pygame(self.body.position, screen)
 
 class Player(Sprite):
-	def __init__(self, position):
+	def __init__(self, position, size):
+		Sprite.__init__(self, position, size)
+		self.image = pygame.image.load('images/apple.png')
+		self.image.convert()
+		self.size = size
+		self.image = pygame.transform.scale(self.image, Vec2d(size)*3)
+		self.faces = [None, pygame.transform.flip(self.image, True, False), self.image]
 		self.gravity = Vec2d(0, -1000)
 		self.mass = 5
 		self.body = pymunk.Body(self.mass, pymunk.inf)
-		self.shape = pymunk.Circle(self.body, 10)
+		self.shape = pymunk.Circle(self.body,size[0])
 		self.body.position = position
-		Sprite.__init__(self, self.body)
+		
 
 		self.actions = {
 			K_LEFT: lambda: self.walk(-1),
@@ -49,6 +60,8 @@ class Player(Sprite):
 	def walk(self, direction):
 		self.direction = direction
 		self.shape.surface_velocity = Vec2d(-self.max_speed*direction, 0)
+		self.image = self.faces[self.direction]
+
 
 
 	def jump(self):
@@ -58,7 +71,7 @@ class Player(Sprite):
 			self.grounded = False
 			self.body.apply_impulse_at_local_point(Vec2d(0, 500))
 
-	def update(self):
+	def update(self, screen):
 
 		# check if object is properly "grounded"
 		self.body.each_arbiter(self.ground_collision)
@@ -71,7 +84,9 @@ class Player(Sprite):
 		for player_key in self.actions:
 			if keys[player_key]:
 				self.actions[player_key]()
-
+		self.pymunk2pygame(screen)
+		self.rect.x -= self.size[0]*1.5
+		self.rect.y -= self.size[1]*1.8
 
 
 
@@ -79,21 +94,29 @@ class Platform(Sprite):
 	# creates a rectangle of the given size
 	# position is top left corner of the rectangle
 	def __init__(self, position, size):
+		Sprite.__init__(self, position, size)
 		self.body = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
 		p = Vec2d(position)
 		s = Vec2d(size)
 		self.shape = pymunk.Poly(self.body, [(p.x, p.y), (p.x, p.y-s.y), (p.x+s.x, p.y-s.y), (p.x+s.x, p.y)])
 		self.shape.friction = 1.
-		Sprite.__init__(self, self.body)
+		
 
 class MovingPlatform(Platform):
 
 	# moves in a path relative to its position
-	def __init__(self, position, size, path):
+	# where a path is a list of points
+	def __init__(self, position, size, path, speed):
 		Platform.__init__(self, position, size)
+		self.path = [Vec2d(point)+self.body.position for point in path]
+		self.dest_index = 0
+		self.destination = self.path[self.dest_index]
 
-	def update(self):
-		print self.body.position
+
+	def update(self, screen):
+		# distance = self.body.position.
+		pass
+		
 
 class Game():
 	def __init__(self):
@@ -120,7 +143,7 @@ class Game():
 		self.dt = 1./self.fps
 
 		# add objects to the game world
-		player = Player((300, 220))
+		player = Player((300, 220), (20, 20))
 		walls = []
 		walls.append(Platform((100, 200), (400, 20)))
 		walls.append(Platform((100, 400), (20, 200)))
@@ -129,6 +152,10 @@ class Game():
 		self.add_sprite(player)
 		for wall in walls:
 			self.add_sprite(wall)
+
+		moving_platform = MovingPlatform((300, 250), (60, 10), [(-40, 0), (40, 0)], 1)
+
+		self.add_sprite(moving_platform)
 
 		# set up debug draw mode
 		# actually displaying real graphics will take some more effort
@@ -148,8 +175,10 @@ class Game():
 				if event.type == QUIT:
 					self.running = False
 
-			self.sprites.update()
-			self.space.debug_draw(draw_options)			
+			self.sprites.update(self.screen)
+			self.space.debug_draw(draw_options)	
+			self.sprites.draw(self.screen)
+					
 
 			self.space.step(self.dt)
 			self.clock.tick(self.fps)
