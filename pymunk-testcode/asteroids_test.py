47930376fdb2e4c4e6a5c4656d77fdb75c1fdbae
Ahmed Ahmed <ahmed.ahmed096@yahoo.com>
2017-06-23 20:06:24 -0400

    Working player character as a triangle and asteroids as circles. Next: bullets

--- pymunk-testcode/asteroids_test.py
+++ pymunk-testcode/asteroids_test.py
@@ -5,20 +5,12 @@ from pygame.color import *
 import pymunk
 from pymunk.vec2d import Vec2d
 import pymunk.pygame_util
-
-def radians_to_vector(angle):
-	
-	# if angle > math.pi:
-	# 	angle = angle % (2 * math.pi )
-	# elif angle < - math.pi:
-	# 	angle = - (abs(angle) % math.pi)
-	# while not (angle >= -math.pi and angle <= math.pi):
-	# 	if angle > math.pi:
-	# 		angle = angle - (2 * math.pi)
-	# 	elif angle < -math.pi:
-	# 		angle = angle + (2 * math.pi)
-
-	return Vec2d((math.cos(angle) ,math.sin(angle)))
+import random 
+"""
+TODO
+- Bullets (key_press --> creation --> collision w/ asteroid --> destroy asteroid & create 2 a fraction of the size)
+    * bullets shouldn't wrap around / should have a timer (check the game)
+"""
 
 class Sprite(pygame.sprite.Sprite):
 	def __init__(self, pymunk_body):
@@ -37,48 +29,33 @@ class Player(Sprite):
 		self.mass = 5
 		self.radius = 10
 		self.body = pymunk.Body(self.mass, pymunk.moment_for_circle(self.mass,0,self.radius),pymunk.Body.DYNAMIC)
-		self.shape = pymunk.Circle(self.body, self.radius)
+		self.shape = pymunk.Poly(self.body, [(-5,0),(5,0),(0,10)])
 		self.body.position = position
-		# self.body_type = pymunk.Body.KINEMATIC
 		self.shape.friction = 1.0
-		print "initial forces:" + str(self.body.force)
-		# set legal button presses
+		
+		# legal buttons for player actions
 		self.legal_actions = [K_UP,K_DOWN,K_LEFT,K_RIGHT]
 
-		# set rocket's forward and turning thrusts (as forces)
-		self.forward_thrust = 20
-		self.turning_speed = 0.05
+		# tune rocket's forward and turning thrust
+		self.forward_thrust = 30 # as an instantaneous force (impulse)
+		self.turning_speed = 0.1 # as radians per step
 
 		Sprite.__init__(self, self.body)
 
 	def move(self, buttons_pressed):
 		# up key applies forward force
 		if K_UP in buttons_pressed and not K_DOWN in buttons_pressed:
-			# thrust = Vec2d((math.cos(self.body.angle % (math.pi)) ,math.sin(self.body.angle % (math.pi)))) *  self.forward_thrust
-			thrust = radians_to_vector(self.body.angle) *  self.forward_thrust
-			print thrust
-			self.body.apply_force_at_local_point(thrust, (0,0))
+			self.body.apply_impulse_at_local_point((0,self.forward_thrust), (0,0))
 
-		# down key decreases velocity at flat rate
+		# down key decreases velocity by 10%
 		elif K_DOWN in buttons_pressed and not K_UP in buttons_pressed:
-			# decrease velocity
 			self.body.velocity = self.body.velocity[0] * 0.9, self.body.velocity[1] * 0.9
 
-
-		# right and left keys turn right and left, respectively, a fixed amount
+		# right and left keys turn right and left, respectively, a constant amount
 		if K_RIGHT in buttons_pressed and not K_LEFT in buttons_pressed:
-			self.body.angle += self.turning_speed
-			# self.body.apply_impulse_at_local_point((self.turning_speed,0), (0,10))
-			# print self.body.angle
-			# print math.cos(self.body.angle) ,math.sin(self.body.angle)
-
-		elif K_LEFT in buttons_pressed and not K_RIGHT in buttons_pressed:
 			self.body.angle -= self.turning_speed
-			# self.body.apply_impulse_at_local_point((-self.turning_speed,0), (0,10))
-			# print self.body.angle
-			# print math.cos(self.body.angle) ,math.sin(self.body.angle)
-
-
+		elif K_LEFT in buttons_pressed and not K_RIGHT in buttons_pressed:
+			self.body.angle += self.turning_speed
 
 	def update(self):
 		# get key press and apply action
@@ -87,24 +64,21 @@ class Player(Sprite):
 		for key in self.legal_actions:
 			if keys_pressed_binary[key]:
 				keys_pressed_list.append(key)
-
 		self.move(keys_pressed_list)
-		# if  K_RIGHT in keys_pressed_list and not K_LEFT in keys_pressed_list:
-		# 	self.body.angular_velocity = 0.
 			
+class Asteroid(Sprite):
+	def __init__(self, size, position, velocity):
+		# set physical properties
+		self.gravity = Vec2d(0, 0)
+		self.mass = 5
+		self.radius = size
+		self.body = pymunk.Body(self.mass, pymunk.moment_for_circle(self.mass,0,self.radius),pymunk.Body.KINEMATIC)
+		self.shape = pymunk.Circle(self.body, self.radius)
+		self.shape.friction = 1.0
+		self.body.velocity = velocity
+		print self.body.velocity
+		self.body.position = position
 
-
-class Wall(Sprite):
-	def __init__(self, position, size):
-		"""
-		creates a rectangle of the given size
-		position is top left corner of the rectangle
-		"""
-		self.body = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
-		p = Vec2d(position)
-		s = Vec2d(size)
-		self.shape = pymunk.Poly(self.body, [(p.x, p.y), (p.x, p.y-s.y), (p.x+s.x, p.y-s.y), (p.x+s.x, p.y)])
-		self.shape.friction = 1.
 		Sprite.__init__(self, self.body)
 
 class Game():
@@ -116,6 +90,35 @@ class Game():
 	def add_sprite(self, sprite):
 		self.sprites.add(sprite)
 		self.space.add(sprite.get_bodies())
+
+	def wrap_around(self,sprites,screen_size):
+		"""
+		Make it so sprites that go off screen teleport to the other side.
+		"""
+		screen_width = screen_size[0]
+		screen_height = screen_size[1]
+		for sprite in self.sprites:
+			x,y = pymunk.pygame_util.to_pygame(sprite.body.position, self.background)
+
+			if x < 0:
+				x = screen_width + x
+			elif x > screen_width:
+				x = x % screen_width 
+			
+			if y < 0:
+				y = screen_height + y
+			elif y > screen_height:
+				y = y % screen_height 
+
+			sprite.body.position = pymunk.pygame_util.from_pygame((x,y), self.background)
+
+	def add_collision_handler(self, sprite1, sprite2, collision_handler, collision_type='begin'):
+		type1 = sprite1.shape.collision_type
+		type2 = sprite2.shape.collision_type
+		if collision_type == 'begin':
+			self.space.add_collision_handler(type1, type2).begin = collision_handler
+		elif collision_type == 'separate':
+			self.space.add_collision_handler(type1, type2).separate = collision_handler
 
 	def run_game(self, size):
 		
@@ -132,34 +135,40 @@ class Game():
 
 		# add player to the game world
 		player = Player((300, 220))
+		player.body.filter = pymunk.ShapeFilter(categories=1)
 		self.add_sprite(player)
 
-		# add walls to the game world
-	
-		# set up debug draw mode
-		# actually displaying real graphics will take some more effort
-		# because the (0, 0) point is in the bottom left (unlike the pygame screen)
+		# add 10 asteroids
+		for _ in range(5):
+			random_position = random.randrange(0,size[0]), random.randrange(0,size[1])
+			random_velocity = random.randrange(-100,100), random.randrange(-100,100)
+			random_size = random.randrange(20,60)
+			asteroid = Asteroid(random_size,random_position,random_velocity)
+			asteroid.body.filter = pymunk.ShapeFilter(categories=2)
+			self.add_sprite(asteroid)
+
+		# add player-asteroid collision handler (nothing happens)
+		self.add_collision_handler(player, asteroid, lambda x, y, z: False)
+
+
+		### Set up debug draw mode ###
+
 		draw_options = pymunk.pygame_util.DrawOptions(self.screen)
-
-		self.running = True
-
-		# create a white background to refresh the screen
 		self.background = pygame.Surface(size)
 		self.background.fill(Color('white'))
 
+		self.running = True
 		while self.running:
 			# refresh the screen
 			self.screen.blit(self.background, (0, 0))
 			for event in pygame.event.get():
 				if event.type == QUIT:
 					self.running = False
-
 			self.sprites.update()
+			self.wrap_around(self.sprites,size)
 			self.space.debug_draw(draw_options)			
-
 			self.space.step(self.dt)
 			self.clock.tick(self.fps)
-
 			pygame.display.flip()
 		pygame.display.quit()
 		sys.exit()
